05bf8715439a58fe9f4e55895efd4df82364177a
imrajashish <imrajashish07@gmail.com>
2022-02-13 23:54:46 +0530

    Created 2 component and added in routing table

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { InterpolationComponent } from './interpolation/interpolation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dataBinding',
+    component: DataBindingComponent
+  },
+  {
+    path: 'interpolation',
+    component: InterpolationComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { InterpolationComponent } from './interpolation/interpolation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataBindingComponent,
+    InterpolationComponent
   ],
   imports: [
     BrowserModule,
